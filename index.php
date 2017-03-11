6595b86830dd4b869f7a3998e1bb3e4d36be656d
Scott <scott@monkeyhouse-media.com>
2017-03-11 07:26:23 -0800

    changed index to php, added tags and footer content

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Gig Harbor's Rockin' Rhythm &amp; Blues Festival</title>
+    <link rel="stylesheet" type="text/css" src="css/style.css">
+  </head>
+
+  </body>
+    <header></header>
+    <section>
+      <h1>Gig Harbor's Rockin' Rhythm &amp; Blues Festival</h1>
+      <h2> Coming soon</h2>
+      <p>URL</p>
+      <h3>Permission to Dream</h3>
+    </section>
+    <footer>
+      &copy; copyright <?php echo date(Y) ?>, All Rights Reserved.
+    </footer>
+  </body>
+</html>
