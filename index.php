2001d3711e562f0fd1bb5f1f4447f4fefb688406
Scott <scott@monkeyhouse-media.com>
2017-03-14 12:38:44 -0700

    updated index and added key imagery

--- index.php
+++ index.php
@@ -3,15 +3,37 @@
   <head>
     <title>Gig Harbor's Rockin' Rhythm &amp; Blues Festival</title>
     <link rel="stylesheet" type="text/css" src="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    <style>
+      body {height:100%; font-family: 'Lato', sans-serif;;}
+      section { margin: 0 auto;width:100%; text-align:center;}
+      .home-image { text-align:center;margin:0 auto; height: 70vh; }
+      .home-image img { height: 100%; }
+      .coming-soon { font-size: 4em }
+      article.ptsd { margin: 2em 0; }
+      .ptsd h1 { margin-top: -10px;}
+      footer { text-align: center; font-size: small; }
+    </style>
   </head>
 
-  </body>
+</body>
     <header></header>
     <section>
-      <h1>Gig Harbor's Rockin' Rhythm &amp; Blues Festival</h1>
-      <h2> Coming soon</h2>
-      <p>URL</p>
-      <h3>Permission to Dream</h3>
+      <div class="home-image">
+        <img src="images/rrbf_logo_main.png" alt="Rockin' Rhythm and Blues Festival" >
+      </div>
+
+      <h2 class="coming-soon"> Coming soon</h2>
+
+      <article class="ptsd">
+        <h3>Raising money to support:</h3>
+        <h1>
+          <a href="http://permissiontostartdreaming.org/" target="_blank">
+            Permission to start Dreaming
+          </a>
+        </h1>
+      </article>
+
     </section>
     <footer>
       &copy; copyright <?php echo date(Y) ?>, All Rights Reserved.
