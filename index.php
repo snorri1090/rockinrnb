5b7d0d482bbbc29e337043e2d73516cce7b24778
Scott <scott@monkeyhouse-media.com>
2017-03-15 14:22:12 -0700

    removed testing page added coming soon index content from website

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
     <link rel="stylesheet" type="text/css" src="css/style.css">
     <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
     <style>
-      body {height:100%; font-family: 'Lato', sans-serif;;}
+      body {height:100vh; font-family: 'Lato', sans-serif;;}
       section { margin: 0 auto;width:100%; text-align:center;}
       .home-image { text-align:center;margin:0 auto; height: 70vh; }
       .home-image img { height: 100%; }
-      .coming-soon { font-size: 4em }
+      .coming-soon { font-size: 4em;text-align: center; height: 70vh }
       article.ptsd { margin: 2em 0; }
       .ptsd h1 { margin-top: -10px;}
       #ptsd_logo_home { width: 100%; max-width: 500px; }
@@ -20,25 +20,10 @@
     </style>
   </head>
 
-</body>
+<body >
     <header></header>
-    <section>
-      <div class="home-image">
-        <img src="images/rrbf_logo_main.png" alt="Rockin' Rhythm and Blues Festival" >
-      </div>
 
-      <h2 class="coming-soon"> Coming soon</h2>
-
-      <article class="ptsd">
-        <div class="supporting">
-          <h3>All proceeds to go support:</h3>
-        </div>
-        <div class="ptsd-logo">
-          <a href="http://permissiontostartdreaming.org/" target="_blank">
-            <img src="images/PTSD_logo_small.png" alt="Permission to Start Dreaming logo" id="ptsd_logo_home">
-          </a>
-        </div>
-      </article>
+      <h1 class="coming-soon"> Coming soon</h1>
 
     </section>
     <footer>
