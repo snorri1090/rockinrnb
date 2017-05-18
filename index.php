6912ea87315b4b6ab883a95b833ce0003c83e47b
Scott <scott@monkeyhouse-media.com>
2017-05-18 09:42:43 -0700

    initial commit, again

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Gig Harbor's Rockin' Rhythm &amp; Blues Festival</title>
+    <link rel="stylesheet" type="text/css" src="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    <style>
+      body {height:100vh; font-family: 'Lato', sans-serif;;}
+      section { margin: 0 auto;width:100%; text-align:center;}
+      .home-image { text-align:center;margin:0 auto; height: 70vh; }
+      .home-image img { height: 100%; }
+      .coming-soon { font-size: 3em }
+      release-1.0.0
+      article.ptsd { margin: 2em 0; }
+      .ptsd h1 { margin-top: -10px;}
+      #ptsd_logo_home { width: 100%; max-width: 300px; }
+      article.ptsd div { }
+      article.ptsd .supporting { }
+      article.ptsd .ptsd-logo {  }
+      .performers { background-color: #000; color: #fff; width: 100%; margin: 0; padding: 0; margin-left: -10; margin-right: -10;}
+      footer { text-align: center; font-size: small; }
+    </style>
+  </head>
+
+<body >
+    <header></header>
+    <section>
+      <h2>Night on the Town Productions</h2>
+      <div class="home-image">
+        <img src="images/rrbf_logo_main.png" alt="Rockin' Rhythm and Blues Festival" >
+      </div>
+
+      <h2 class="coming-soon" style="margin-bottom: -15px;"> Coming August 12, 2017</h2>
+
+      <h3>Gig Harbor Sportsman's Club<br>
+      <small>9721 Burnham Drive NW</small><br>
+      FREE PARKING
+      </h3>
+
+      <h3>12 Noon - 9PM</h3>
+
+      <div class="performers" >
+      <h2>
+        Featured Performers:
+      </h2>
+        <ul>
+          <li>Kal David and the Real Deal Band feature Lauri Bono</li>
+          <li>The Shy Boys</li>
+          <li>Mark Hurwitz &amp; Gin Creek</li>
+          <li>The Twang Junkies</li>
+          <li>Jim Valley Blues Band</li>
+          <li>The Whirlies</li>
+          <li>King Kong Beaux</li>
+        </ul>
+    </div>
+
+<hr>
+<div>Ticket outlets and details coming soon</div>
+
+
+
+
+      <hr>
+      <article class="ptsd">
+        <div class="supporting">
+          <h3>All proceeds to go support:</h3>
+        </div>
+        <div class="ptsd-logo">
+          <a href="http://permissiontostartdreaming.org/" target="_blank">
+            <img src="images/PTSD_logo_small.png" alt="Permission to Start Dreaming logo" id="ptsd_logo_home">
+          </a>
+        </div>
+      </article>
+
+    </section>
+    <footer>
+      &copy; copyright <?php echo date(Y) ?>, All Rights Reserved.
+    </footer>
+<!--<p style="font-weight:bold;font-size: 20px;">More information to follow</p> -->
+
+  </body>
+</html>
